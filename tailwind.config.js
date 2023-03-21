a596cf2725f219443ca2693fd2e7250cebcd9316
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-03-21 05:38:51 +0300

    add IBM Plex Sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        IBMPlexSans: 'IBM Plex Sans',
+      },
+    },
   },
   plugins: [],
 };
