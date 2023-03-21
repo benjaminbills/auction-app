72cbdd2dfaeb66f687b16eb13a074dcea816a8e1
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-03-21 10:24:49 +0300

    fetch data with react query

--- src/components/AuctionCardsList.jsx
+++ src/components/AuctionCardsList.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import axios from 'axios';
 const AuctionCardsList = () => {
+  const { isLoading, data } = useQuery('auction-list', () => {
+    return axios.get(
+      'https://run.mocky.io/v3/7f02819f-8254-410a-b8af-ab98572bd26b'
+    );
+  });
+  if (isLoading) {
+    return <h2>Loading</h2>;
+  }
+  console.log(data);
   return (
     <div>
       <p>Featured Items</p>
