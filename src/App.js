318c231918ce7033f9387b1c37d1402a69951428
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-03-21 10:24:31 +0300

    setup react query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient();
 function App() {
   return (
-    <div className='pt-[32px] pl-[32px] pr-[32px] pb-[48px] font-IBMPlexSans'>
-      <Home />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='pt-[32px] pl-[32px] pr-[32px] pb-[48px] font-IBMPlexSans'>
+        <Home />
+      </div>
+    </QueryClientProvider>
   );
 }
 
