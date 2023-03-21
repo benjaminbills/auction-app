042d9dd983593af9df3cf4909e02901b4a20dd46
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-03-21 06:47:46 +0300

    added header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Notification from './Notification';
+
+const Header = () => {
+  return (
+    <div className='flex flex-col'>
+      <div className='flex justify-between '>
+        <p className='font-semibold text-[30px] text-gray-900'>Welcome</p>
+        <Notification />
+      </div>
+      <p className='text-base font-normal text-gray-600'>
+        Your current sales auction and activity.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
