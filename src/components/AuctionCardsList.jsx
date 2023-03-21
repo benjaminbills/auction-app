19f5aec9b485ab3accd484d36c5866c08949296d
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-03-21 09:58:58 +0300

    create auction card list

---
+++ src/components/AuctionCardsList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+const AuctionCardsList = () => {
+  return (
+    <div>
+      <p>Featured Items</p>
+      <div></div>
+    </div>
+  );
+};
+
+export default AuctionCardsList;
