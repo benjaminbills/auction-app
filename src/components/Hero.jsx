99c00c8982b282399cc99e4efa216f65eb5ad0be
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-03-21 08:34:29 +0300

    add hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='bg-white w-full h-[332px] rounded-xl mt-[48px] border border-gray-200'>
+      <div className='p-[24px]'>
+        <div className='bg-[url("../public/images/image.svg")] h-[192px] bg-cover bg-no-repeat rounded-xl'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
