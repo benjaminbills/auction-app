280499b9835dff7b08d2343f00fa11bf96995e88
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-03-21 06:48:18 +0300

    create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <hr className='mt-[20px]' />
+    </div>
+  );
+};
+
+export default Home;
