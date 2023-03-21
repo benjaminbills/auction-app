7a4151b3a3912d62f0519e9744ad7a68b8e90b2c
Benjamin Obafemi <benjaminoba234@gmail.com>
2023-03-21 14:36:20 +0300

    map data to auction card

--- src/components/AuctionCardsList.jsx
+++ src/components/AuctionCardsList.jsx
@@ -13,9 +13,15 @@ const AuctionCardsList = () => {
   }
   console.log(data);
   return (
-    <div>
-      <p>Featured Items</p>
-      <div>
+    <div className='border-1 border rounded-xl p-2 sm:p-6 mt-6 '>
+      <div className='flex justify-between items-center  pb-[24px]'>
+        <p className='text-gray-900 text-lg font-semibold'>Featured Items</p>
+        <button className='border border-1 border-gray-300 pt-[8px] pb-[8px] pl-[14px] pr-[14px] font-semibold text-sm text-gray-700 rounded-lg'>
+          View Auction
+        </button>
+      </div>
+      <hr className='pb-[24px]' />
+      <div className='grid min-[1250px]:grid-cols-4 lg:grid-cols-3 gap-6 grid-cols-1 md:grid-cols-2 sm:grid-cols-2'>
         {data.data.data.map((item, i) => (
           <AuctionCard key={i} item={item} />
         ))}
